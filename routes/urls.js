a020cbab183654a3b5879f9ae49c902638bf4164
RyomenDev <aakashvatsh007@gmail.com>
2024-06-09 15:31:57 +0530

    minor

--- routes/urls.js
+++ routes/urls.js
@@ -1,30 +1,30 @@
 const { readDocument } = require("../utils/fileUtils");
 const path = require("path");
 
-// Declare a variable to store the content of the URLs file
-let urlsstr;
+// Declare a variable to store the content of the titles file
+let titlesstr;
 
 try {
-  // Define the file path to the problem URLs file
-  const filePath = path.join(__dirname, "../problem-urls.txt");
+  // Define the file path to the problem titles file
+  const filePath = path.join(__dirname, "../problem-titles.txt");
 
-  // Read the content of the problem URLs file using the readDocument utility function
-  urlsstr = readDocument(filePath);
+  // Read the content of the problem titles file using the readDocument utility function
+  titlesstr = readDocument(filePath);
 
   // Alternatively, using fs.readFileSync to read the file content directly
-  // urlsstr = fs.readFileSync(filePath).toString();
+  // titlesstr = fs.readFileSync(filePath).toString();
 } catch (error) {
   // Handle the file not found error specifically
   if (error.code === "ENOENT") {
-    console.error("The file 'problem-urls.txt' does not exist.");
+    console.error("The file 'problem-titles.txt' does not exist.");
   } else {
     // Handle any other errors that occur while reading the file
-    console.error("Error reading problem-urls file:", error);
+    console.error("Error reading problem-titles file:", error);
   }
 }
 
-// Split the file content into an array of URLs, each line representing a URL
-const urls = urlsstr.split("\n");
+// Split the file content into an array of titles, each line representing a title
+const titles = titlesstr.split("\n");
 
-// Export the urls array for use in other modules
-module.exports = urls;
+// Export the titles array for use in other modules
+module.exports = titles;
